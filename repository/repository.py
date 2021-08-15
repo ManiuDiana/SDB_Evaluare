82c4ac6a35747e1b5320e5421dad53c56d18dcbb
MM <dianamaniu@gmail.com>
2021-08-15 20:11:24 +0300

    Added repository and changed structure

---
+++ repository/repository.py
@@ -0,0 +1,12 @@
+class Repository:
+    def __init__(self):
+        self.__entities_list = []
+
+    def add(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def show_all(self):
+        pass
